e35f053c28e976c61d9d6b1a280e5cec98f21fab
Cody Northrop <cnorthrop@google.com>
2024-07-22 22:24:10 +0800

    config: Configure EGL multi-file cache
    Start with 32MB cache size limit.
    
    Bug: 291316306
    Change-Id: Ib917c3199ac9a205d72ae6c32c0ee82b4b0aebe7

--- config/common.mk
+++ config/common.mk
@@ -259,3 +259,8 @@ include vendor/lineage/config/version.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/lineage/config/partner_gms.mk
+
+# EGL - Blobcache configuration
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.egl.blobcache.multifile=true \
+    ro.egl.blobcache.multifile_limit=33554432
